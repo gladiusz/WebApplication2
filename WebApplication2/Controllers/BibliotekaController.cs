2bbca349f0091284180ea92651b1e633d4f3b1d3
gladiusz <gladi962011@gmail.com>
2018-04-20 16:28:55 +0200

    Lab 1,2 - widoki zorientowane na model

---
+++ WebApplication2/Controllers/BibliotekaController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class BibliotekaController : Controller
+    {
+        static List<KsiazkaModel> data = new List<KsiazkaModel>();
+
+
+        public ActionResult Index()
+        {
+            return View(data);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            data.RemoveAt(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            return View(id != null
+                ? data[(int)id]
+                : new KsiazkaModel());
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id, KsiazkaModel ksiazka)
+        {
+            if (id == null)
+                data.Add(ksiazka);
+            else
+                data[(int)id] = ksiazka;
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ WebApplication2/Models/KsiazkaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class KsiazkaModel
+    {
+        public enum OkladkaTyp { brakInfo, twarda, miekka };
+
+        public string Tytul { get; set; }
+        public string Autor { get; set; }
+        public int IloscStron { get; set; }
+        public OkladkaTyp Okladka { get; set; }
+    }
+}
